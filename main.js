bf291cccf7ee3da399027eab865a7a53272dbab3
Ester Sanchez <esthersanchez95@gmail.com>
2021-10-18 08:30:48 +0200

    dom

--- main.js
+++ main.js
@@ -1,42 +1,59 @@
-//DESAFIO 4. CALCULO DE PRECIO TOTAL SUMANDO
-
-// let precioTotal = 0;
-// let precioProducto;
-
-// function sumaProductos(precioProducto) {
-//     while (precioProducto != '') {
-//         precioProducto =  Number(prompt('Escribe el precio del producto que quieres'));
-//         precioTotal = precioTotal + precioProducto;
-//         console.log(precioProducto);
-//       };
+let productosJSON = [
+    {
+      "photo": "img/botaniste.jpg",
+      "titulo": "Botaniste",
+      "precio": 35.75,
+      "tipo": 'Perfume de jazmín'
+    },
+    {
+       "photo": "img/perfume.jpg",
+        "titulo": "Lavender",
+        "precio": 35.75,
+        "tipo": 'Perfume de lavanda'
+    },
+    {
+       "photo": "img/jabon.jpg",
+       "titulo": "Savon",
+       "precio": 9.95,
+       "tipo": 'Jabón sólido'
+     },
+     {
+         "photo": "img/cafe.jpg",
+         "titulo": "Café",
+         "precio": 9.95,
+         "tipo": 'Jabón sólido'
+     },
+     {
+       "photo": "img/dientes.jpg",
+       "titulo": "Organic",
+       "precio": 5.50,
+       "tipo": 'Cepillo de dientes'
+     },
+     {
+         "photo": "img/spray.jpg",
+         "titulo": "Kalos",
+         "precio": 7.90,
+         "tipo": 'Spray corporal'
+     },
+     {
+       "photo": "img/jade.jpg",
+       "titulo": "Jade",
+       "precio": 14.10,
+       "tipo": 'Rodillo de jade'
+     },
+     {
+         "photo": "img/cepillos.jpg",
+         "titulo": "Body brush",
+         "precio": 4.90,
+         "tipo": 'Cepillo corporal'
+     },
     
-//       alert('la suma total de los productos es de: ' + precioTotal + '€'); 
-// };
-
-// sumaProductos();
+  ];
 
 
-// //DESAFIO COMPLEMENTARIO. CALCULO DEL IVA DEL 21%
-// let precioConIVA;
+window.addEventListener('load', () => {
+    console.log('DOM Cargado');
 
-// function sumaIVA(){
-//     precioConIVA = precioTotal + 1.21;
-    
-// }
-
-// sumaIVA();
-
-// function mostrarPrecioIVA(){
-//     console.log('El precio con IVA asciende a: ' + precioConIVA + '€');
-// }
-
-// mostrarPrecioIVA();
-
-
-
-
-
-/////////////////////////////// Clase 5
 //construcción clase de productos
 
 class Producto {
@@ -44,84 +61,50 @@ class Producto {
     constructor( titulo, precio, tipo) {
     
         this.titulo = titulo;
-        this.precio = precio;
+        this.precio = parseFloat(precio);
         this.tipo = tipo;
 
     }
 
     getPrecioConIva() {
-        return this.precio * 1.21
+        return this.precio * 1.21;
     }
 
     getPrecio() {
-        return this.precio
+        return this.precio;
     }
-
 }
 
 
-const producto1 = new Producto('Botaniste', 35.75, 'Perfume de jazmín')
-const producto2 = new Producto('Lavender', 35.75, 'Perfume de lavanda')
-
-
+var productosArray= []
 //añadir producto por prompt
 function añadirProducto() {
     const titulo = prompt("indica el nombre del producto");
     const precio = prompt("indica el precio");
     const tipo = prompt("indica el tipo de producto");
-  
-    var producto3 = new Producto(titulo, precio, tipo);
-    console.log(producto3);
 
-    console.log(producto3.getPrecio());
-    console.log(producto3.getPrecioConIva())
+    productosArray.push(new Producto(titulo, precio, tipo));
+    console.log(productosArray);
   }
 
 añadirProducto();
 
-/////////////////////////////// Clase 6
-//Agregar array
-const nombre_articulos = ['Botaniste', 'Lavender', 'Savon', 'Café', 'Organic', 'Kalos', 'Jade', 'Body Brush'];
+const productContainer = document.getElementById('productContainer')
 
-for( let i = 0; i < nombre_articulos.length; i++) {
-    
-    console.log(nombre_articulos[i]);
+
+for (let producto of productosArray) {
+    let article = document.createElement('article');
+    article.innerHTML = `<img src="img/botaniste.jpg">
+    <div id="caracteristicas">
+        <h6 id="titulo">${producto.titulo}</h6>
+        <p>${producto.tipo}</p>
+        <p id="precio">${producto.precio}</p>
+    </div>
+    <button class="boton_añadir"><a href="#">AÑADIR AL CARRITO</a></button>`;
+
+    productContainer.appendChild(article);
 }
 
-//buscar articulos que contengan la letra e y los mete en un array
-const e = nombre_articulos.filter(elemento => elemento.includes("e"));
-console.log(e);   
 
-//meter un nuevo elemento en el array
-nuevos_articulos= nombre_articulos.push('Cepillo');
 
-for( let x = 0; x < nuevos_articulos.length; x++) {
-    
-    console.log(nuevos_articulos[x]);
-};
-
-//////////////////////////////// desafio complementario crear array de objetos
-let articulos = [
-    {
-      "nombre": "botaniste",
-      "precio": 35.75,
-      "tipo": 'perfume'
-    },
-    {
-        "nombre": "lavender",
-        "precio": "35.75",
-        "tipo": 'perfume'
-    },
-    {
-        "nombre": "savon",
-        "precio": 9.95,
-        "tipo": 'jabon'
-      },
-      {
-          "nombre": "cafe",
-          "precio": 9.95,
-          "tipo": 'jabon'
-      }
-  ];
-
- console.log(articulos.sort((a,b) => (a.precio > b.precio) ? 1 : ((b.precio > a.precio) ? -1 : 0)));
+}); //final del load

45153b329ddc76d9ded7027b5b88f1844fa371e8
Ester Sanchez <esthersanchez95@gmail.com>
2021-10-24 17:56:46 +0200

    añadido carrito compras

--- main.js
+++ main.js
@@ -58,11 +58,12 @@ window.addEventListener('load', () => {
 
 class Producto {
 
-    constructor( titulo, precio, tipo) {
+    constructor( titulo, precio, tipo, photo) {
     
         this.titulo = titulo;
         this.precio = parseFloat(precio);
         this.tipo = tipo;
+        this.photo = photo;
 
     }
 
@@ -76,33 +77,95 @@ class Producto {
 }
 
 
-var productosArray= []
-//añadir producto por prompt
-function añadirProducto() {
-    const titulo = prompt("indica el nombre del producto");
-    const precio = prompt("indica el precio");
-    const tipo = prompt("indica el tipo de producto");
-
-    productosArray.push(new Producto(titulo, precio, tipo));
-    console.log(productosArray);
-  }
-
-añadirProducto();
-
+//fichas de producto a partir del json
 const productContainer = document.getElementById('productContainer')
 
 
-for (let producto of productosArray) {
+for (let producto of productosJSON) {
     let article = document.createElement('article');
-    article.innerHTML = `<img src="img/botaniste.jpg">
+    article.className= 'item';
+    article.innerHTML = `<img id="foto" src="${producto.photo}">
     <div id="caracteristicas">
         <h6 id="titulo">${producto.titulo}</h6>
-        <p>${producto.tipo}</p>
+        <p id="tipo">${producto.tipo}</p>
         <p id="precio">${producto.precio}</p>
     </div>
     <button class="boton_añadir"><a href="#">AÑADIR AL CARRITO</a></button>`;
 
     productContainer.appendChild(article);
+}
+
+
+//eventos
+
+const carrito = document.getElementById('carrito');
+const shoppingCartRow = document.getElementById('lista-carrito');
+const botonAnadir = document.getElementsByClassName('boton_añadir');
+const vaciarCarritoBtn = document.getElementsByClassName('vaciar-carrito');
+
+cargarEventListeners();
+
+//Funcion de todos los eventos
+function cargarEventListeners(){
+
+    for (var i = 0 ; i < productosJSON.length; i++) {
+        botonAnadir[i].addEventListener('click', comprarProducto);
+    };
+}
+    
+
+//Evento botón añadir al carrito
+function comprarProducto(event){
+    const but = event.target;
+    const item = but.closest('.item');
+    
+    const itemTitle = item.querySelector('#titulo').textContent;
+    const itemTipo = item.querySelector('#tipo').textContent;
+    const itemPrice = item.querySelector('#precio').textContent;
+
+    addItemToShoppingCart(itemTitle, itemTipo, itemPrice);
+}
+
+function addItemToShoppingCart (itemTitle, itemTipo, itemPrice){
+    const shoppingCartContent = `
+                               <tbody class= "shoppingCartItem">
+                                    <tr>  
+                                        <td>${itemTitle}</td>
+                                        <td>${itemTipo}</td>
+                                        <td class="shoppingCartItemPrice">${itemPrice}€</td>
+                                        <input class="shoppingCartItemQuantity" type="number" value="1">
+                                    </tr>
+                                </tbody>
+                                `
+    shoppingCartRow.innerHTML += shoppingCartContent;
+    carrito.append(shoppingCartRow);
+
+    updateShoppingCartTotal();
+}
+
+//actualizar total carrito
+function updateShoppingCartTotal(){
+    let total=0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
+
+    const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
+    
+    shoppingCartItems.forEach((shoppingCartItem) => {
+        const shoppingCartItemPriceElement = shoppingCartItem.querySelector('.shoppingCartItemPrice');
+        const shoppingCartItemPrice = Number(shoppingCartItemPriceElement.textContent.replace('€', ''));
+
+        var shoppingCartItemQuantityElement = shoppingCartItem.querySelector('.shoppingCartItemQuantity')||"";
+        if (shoppingCartItemQuantityElement=="")
+            {
+                document.getElementsByClassName("shoppingCartItemQuantity").innerHTML="";
+                return;
+            }; 
+        
+        const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value);
+        console.log(shoppingCartItemQuantityElement);
+        total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
+      });
+
 }
 
 
